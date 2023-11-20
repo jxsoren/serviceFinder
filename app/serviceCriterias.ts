0d76ae7365ef5cdd3686f65902612a1ce50b15d8
jxsoren <jxsoren@gmail.com>
2023-11-19 17:23:41 -0700

    added addtional USPS services

--- app/serviceCriterias.ts
+++ app/serviceCriterias.ts
@@ -18,7 +18,7 @@ const domestic = [
   },
   {
     service: "USPS First Class",
-    subService: "USPS First Class Mail Letter",
+    subService: "USPS First Class Mail Thick Envelope",
     weightLimit: 16,
     maxLength: 15,
     maxWidth: 12,
@@ -26,6 +26,78 @@ const domestic = [
     minLength: 11.5,
     minWidth: 6.125,
     minHeight: 0.25,
+  },
+  {
+    service: "USPS Media Mail",
+    subService: "USPS Media Mail",
+    weightLimit: 1120,
+    maxLengthPlusGirth: 108,
+  },
+  {
+    service: "USPS Parcel Select",
+    subService: "USPS Parcel Select (weight-based)",
+    weightLimit: 1120,
+    maxLengthPlusGirth: 130,
+  },
+  {
+    service: "USPS Priority Mail",
+    subService: "USPS Priority Mail (parcel weight-based)",
+    weightLimit: 1120,
+    maxLengthPlusGirth: 108,
+  },
+  {
+    service: "USPS Priority Mail",
+    subService: "USPS Priority Mail Cubic",
+    weightLimit: 320,
+    maxLength: 18,
+  },
+  {
+    service: "USPS Priority Mail",
+    subService: "USPS Priority Mail Flat Rate Envelope",
+    weightLimit: 1120,
+    maxLength: 12.5,
+    maxWidth: 9.5,
+  },
+  {
+    service: "USPS Priority Mail",
+    subService: "USPS Priority Mail Flat Rate Legal Envelope",
+    weightLimit: 1120,
+    maxLength: 15,
+    maxWidth: 9.5,
+  },
+  {
+    service: "USPS Priority Mail",
+    subService: "USPS Priority Mail Flat Rate Padded Envelope",
+    weightLimit: 1120,
+    maxLength: 11.625,
+    maxWidth: 15,
+  },
+
+  {
+    service: "USPS Priority Mail",
+    subService: "USPS Priority Mail Large Flat Rate Box",
+    weightLimit: 1120,
+    maxLength: 12.25,
+    maxWidth: 12,
+    maxHeight: 6,
+  },
+
+  {
+    service: "USPS Priority Mail",
+    subService: "USPS Priority Mail Medium Flat Rate Box",
+    weightLimit: 240,
+    maxLength: 12.25,
+    maxWidth: 12,
+    maxHeight: 6,
+  },
+
+  {
+    service: "USPS Priority Mail",
+    subService: "USPS Priority Mail Medium Flat Rate Box",
+    weightLimit: 240,
+    maxLength: 12.25,
+    maxWidth: 12,
+    maxHeight: 6,
   },
 ];
 
