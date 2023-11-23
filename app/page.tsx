acd6502e7ec4e845e013df8f1852655fb98a0ff4
jxsoren <jxsoren@gmail.com>
2023-11-23 13:01:03 -0700

    overhaul to UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,31 @@
-"use client";
-
-import {
-  Box,
-  Heading,
-  Text,
-  Link,
-  Stack,
-  Input,
-  Center,
-} from "@chakra-ui/react";
-
+import React from "react";
+import { Box, Heading, Stack, Center, VStack, Fade } from "@chakra-ui/react";
 import Inputs from "./Components/Inputs";
+import { FaCalculator } from "react-icons/fa";
 
 export default function Home() {
   return (
-    <Stack>
-      <Center>
-        <Heading as="h1" size="2xl" mb={4}>
-          Service Calculator
-        </Heading>
-      </Center>
-      <Inputs />
-    </Stack>
+    <Box
+      bgGradient="linear(to-r, blue.400, teal.300)"
+      minH="100vh"
+      py={10}
+      px={5}
+    >
+      <VStack spacing={8} align="stretch">
+        <Center>
+          <Fade in={true} delay={0.5}>
+            <Heading as="h1" size="2xl" mb={4} color="white">
+              <FaCalculator
+                style={{ display: "inline", marginRight: "10px" }}
+              />
+              Service Calculator
+            </Heading>
+          </Fade>
+        </Center>
+        <Center>
+          <Inputs />
+        </Center>
+      </VStack>
+    </Box>
   );
 }
