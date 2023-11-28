8f16a01f39736a6b05f0593bdb270b3c83b82b55
jxsoren <jxsoren@gmail.com>
2023-11-27 21:34:20 -0700

    Changed BG color + removed fade animation com

--- app/page.tsx
+++ app/page.tsx
@@ -7,21 +7,17 @@ import ServiceCalculator from "./Components/ServiceCalculator";
 export default function Home() {
   return (
     <Box
-      bgGradient="linear(to-r, blue.400, teal.300)"
+      bgGradient="linear(to-r, #1E5DF9, #30E3CA)"
       minH="100vh"
       py={10}
       px={5}
     >
       <VStack spacing={8} align="stretch">
         <Center>
-          <Fade in={true} delay={0.5}>
-            <Heading as="h1" size="2xl" mb={4} color="white">
-              <FaCalculator
-                style={{ display: "inline", marginRight: "10px" }}
-              />
-              Service Calculator
-            </Heading>
-          </Fade>
+          <Heading as="h1" size="2xl" mb={4} color="white">
+            <FaCalculator style={{ display: "inline", marginRight: "10px" }} />
+            Service Calculator
+          </Heading>
         </Center>
         <Center>
           <ServiceCalculator />
