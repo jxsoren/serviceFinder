2f8fcdf2ee7828043a0e6afdda524a6a1ed9996e
jxsoren <jxsoren@gmail.com>
2023-11-27 21:35:15 -0700

    added provider icons for each service

--- app/Components/ServiceItem.tsx
+++ app/Components/ServiceItem.tsx
@@ -1,29 +1,68 @@
+import React from "react";
+
+import { Box, Text, Flex, Icon, useColorModeValue } from "@chakra-ui/react";
 import {
-  Box,
-  Text,
-  Flex,
-  Icon,
-  useColorModeValue,
-  Tooltip,
-} from "@chakra-ui/react";
-import { FaTruck, FaClock, FaInfoCircle } from "react-icons/fa";
+  FaTruck,
+  FaClock,
+  FaInfoCircle,
+  FaUsps,
+  FaUps,
+  FaFedex,
+  FaDhl,
+} from "react-icons/fa";
+import { IconType } from "react-icons";
 
 interface ServiceItemProps {
   service: string;
   subService?: string;
   additionalDetails?: { transitTime: string; isGround: boolean };
+  provider: string;
+}
+
+interface ProviderIcons {
+  USPS: IconType;
+  UPS: IconType;
+  FedEx: IconType;
+  DHL: IconType;
+  [key: string]: IconType;
+}
+
+interface ProviderColors {
+  USPS: string;
+  UPS: string;
+  FedEx: string;
+  DHL: string;
+  [key: string]: string;
 }
 
 const ServiceItem: React.FC<ServiceItemProps> = ({
   service,
   subService,
   additionalDetails,
+  provider,
 }) => {
   const bgColor = useColorModeValue("gray.100", "gray.700");
   const borderColor = useColorModeValue("gray.200", "gray.600");
   const textColor = useColorModeValue("gray.800", "whiteAlpha.900");
 
   const { transitTime, isGround } = additionalDetails || {};
+
+  const providerIcons: ProviderIcons = {
+    USPS: FaUsps,
+    UPS: FaUps,
+    FedEx: FaFedex,
+    DHL: FaDhl,
+  };
+
+  const providerColors: ProviderColors = {
+    USPS: "blue.500",
+    UPS: "yellow.500",
+    FedEx: "purple.500",
+    DHL: "red.500",
+  };
+
+  let providerIcon: IconType = providerIcons[provider] || FaInfoCircle;
+  let providerColor: string = providerColors[provider] || "gray.500";
 
   return (
     <Box
@@ -38,8 +77,8 @@ const ServiceItem: React.FC<ServiceItemProps> = ({
       transition="all 0.2s ease-in-out"
     >
       <Flex alignItems="center" mb={3}>
-        <Icon as={FaInfoCircle} color="blue.500" mr={2} />
-        <Text fontSize="lg" fontWeight="semibold" color={textColor}>
+        <Icon as={providerIcon} color={providerColor} mr={2} />
+        <Text fontSize="lg" fontWeight="semizzbold" color={textColor}>
           {service}
         </Text>
       </Flex>
