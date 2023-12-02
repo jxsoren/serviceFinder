8813c58d9310af42d627697acf65dcc2139b2c82
Josh Sorensen <jxsoren@gmail.com>
2023-12-02 16:43:02 -0700

    Refactor page component layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,26 @@
 import React from "react";
-import {
-  Box,
-  Heading,
-  Stack,
-  Center,
-  VStack,
-  Fade,
-  Text,
-} from "@chakra-ui/react";
+import { Box, Heading, VStack, Text } from "@chakra-ui/react";
 
 export default function Home() {
   return (
-    <Box>
-      <Text>Home</Text>
+    <Box minH="100vh" p={4}>
+      <VStack
+        spacing={6}
+        align="stretch"
+        maxW="xl"
+        mx="auto"
+        p={8}
+        boxShadow="lg"
+        borderRadius="lg"
+        bg="white"
+      >
+        <Heading as="h1" size="xl" textAlign="center">
+          Service Explorer Tool
+        </Heading>
+        <Text color="gray.500" textAlign="center">
+          :D
+        </Text>
+      </VStack>
     </Box>
   );
 }
