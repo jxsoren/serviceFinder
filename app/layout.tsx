5245fbf1e411f5705d8bc35131512ff0eef600d2
jxsoren <jxsoren@gmail.com>
2023-12-02 14:41:19 -0700

    layout change

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
 import { Providers } from "./providers";
+import { Box } from "@chakra-ui/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,15 +11,28 @@ export const metadata: Metadata = {
   description: "Calculate shipping services and rates for your package.",
 };
 
+import Navbar from "./Components/Nav/Navbar";
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className} style={{ backgroundColor: "#1E5DF9" }}>
-        <Providers>{children}</Providers>
+    <html>
+      <head>
+        <title>Service Calculator</title>
+      </head>
+      <body>
+        <Box
+          bgGradient="linear(to-r, #1E5DF9, #30E3CA)"
+          minH="100vh"
+          py={10}
+          px={5}
+        >
+          <Navbar />
+          <Providers>{children}</Providers>
+        </Box>
       </body>
     </html>
   );
