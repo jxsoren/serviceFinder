3d97c2fd51cbe16bec7986279c236c2c37ad4f65
jxsoren <jxsoren@gmail.com>
2023-12-02 14:42:01 -0700

    stripped of layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,18 @@
 import React from "react";
-import { Box, Heading, Stack, Center, VStack, Fade } from "@chakra-ui/react";
-import { FaCalculator } from "react-icons/fa";
-
-import ServiceCalculator from "./Components/ServiceCalculator/ServiceCalculator";
+import {
+  Box,
+  Heading,
+  Stack,
+  Center,
+  VStack,
+  Fade,
+  Text,
+} from "@chakra-ui/react";
 
 export default function Home() {
   return (
-    <Box
-      bgGradient="linear(to-r, #1E5DF9, #30E3CA)"
-      minH="100vh"
-      py={10}
-      px={5}
-    >
-      <VStack spacing={8} align="stretch">
-        <Center>
-          <Heading as="h1" size="2xl" mb={4} color="white">
-            <FaCalculator style={{ display: "inline", marginRight: "10px" }} />
-            Service Calculator
-          </Heading>
-        </Center>
-        <Center>
-          <ServiceCalculator />
-        </Center>
-      </VStack>
+    <Box>
+      <Text>Home</Text>
     </Box>
   );
 }
