db2d1b8d07b117eac8787f7e817638e39662883d
jxsoren <jxsoren@gmail.com>
2024-01-18 22:50:27 -0700

    hooked up links to navigation buttons

--- app/page.tsx
+++ app/page.tsx
@@ -13,6 +13,9 @@ import {
 import { IconType } from "react-icons";
 import { FaCalculator, FaCompass, FaSearch, FaTable } from "react-icons/fa";
 
+import { usePathname } from "next/navigation";
+import { Link } from "@chakra-ui/next-js";
+
 import PageHeader from "./Components/PageHeader";
 
 interface FeatureCardProps {
@@ -20,16 +23,12 @@ interface FeatureCardProps {
   title: string;
   text: string;
   action: () => void;
+  navLink: string;
 }
 
 const Home: React.FC = () => {
   return (
-    <Box
-      bgGradient="linear(to-r, #1E5DF9, #30E3CA)"
-      minH="80vh"
-      py={10}
-      px={5}
-    >
+    <Box bgGradient="linear(to-r, #1E5DF9, #30E3CA)" minH="80vh" py={10} px={5}>
       <PageHeader icon={FaCompass} title={"Service Explorer Tool"} />
 
       <VStack spacing={10} mt={10} textAlign="center">
@@ -51,16 +50,42 @@ const Home: React.FC = () => {
             title="Service Lookup"
             text="Identify services by ID and browse details."
             action={() => console.log("Navigate to Service Lookup")}
+            navLink="/serviceLookup"
           />
           <FeatureCard
             icon={FaCalculator}
             title="Service Qualification"
             text="Check service availability for a shipment."
             action={() => console.log("Navigate to Cost Calculator")}
+            navLink="/serviceCalculator"
           />
         </Flex>
       </VStack>
     </Box>
+  );
+};
+
+interface NavigationLinkProps {
+  path: string;
+}
+
+const NavigationLink: React.FC<NavigationLinkProps> = ({ path }) => {
+  return (
+    <Link
+      href={path}
+      _hover={{ textDecoration: "none" }}
+      _focus={{ boxShadow: "none" }}
+    >
+      <Button
+        size="sm"
+        colorScheme="blue"
+        variant="outline"
+        borderColor="whiteAlpha.800"
+        _hover={{ bg: "blue.500", borderColor: "blue.500" }}
+      >
+        Get Started
+      </Button>
+    </Link>
   );
 };
 
@@ -69,6 +94,7 @@ const FeatureCard: React.FC<FeatureCardProps> = ({
   title,
   text,
   action,
+  navLink,
 }) => {
   return (
     <VStack
@@ -79,22 +105,13 @@ const FeatureCard: React.FC<FeatureCardProps> = ({
       boxShadow="xl"
       _hover={{ bg: "whiteAlpha.400", cursor: "pointer" }}
       transition="background 0.3s ease"
-      onClick={action}
     >
       <Icon as={icon} w={16} h={16} color="white" />
       <Heading color="white" as="h3" size="md">
         {title}
       </Heading>
       <Text color="whiteAlpha.800">{text}</Text>
-      <Button
-        size="sm"
-        colorScheme="blue"
-        variant="outline"
-        borderColor="whiteAlpha.800"
-        _hover={{ bg: "blue.500", borderColor: "blue.500" }}
-      >
-        Learn More
-      </Button>
+      <NavigationLink path={navLink} />
     </VStack>
   );
 };
