6ab1541d41224b40b3d1509096cce68faf7cda45
jxsoren <jxsoren@gmail.com>
2024-01-20 19:31:58 -0700

    removed the auth obj, due to presence if jwt strat

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -13,18 +13,6 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: GOOGLE_CLIENT_ID,
       clientSecret: GOOGLE_CLIENT_SECRET,
-      /*
-        Google only provides Refresh Token to an application the first time a user signs in.
-        The authorization object is used to force a new Refresh Token to be generated on each sign in, 
-        as no database is being used to persist user accounts.
-      */
-      authorization: {
-        params: {
-          prompt: "consent",
-          access_type: "offline",
-          response_type: "code",
-        },
-      },
     }),
   ],
 };
